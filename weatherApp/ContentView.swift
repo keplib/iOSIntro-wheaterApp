2d1cabe8d830eeefb91bb5844d8d13a0f3a8ed0a
keplib <keplib@gmail.com>
2024-08-28 15:07:46 +0200

    Create UI for the app

--- weatherApp/ContentView.swift
+++ weatherApp/ContentView.swift
@@ -8,17 +8,106 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            BackgroundView(isNight: isNight)
+            
+            VStack {
+                CityTextView(cityName: "Cupertino, CA")
+                MainWeatherStatusView(isNight: isNight, temperature: 76)
+                HStack(spacing: 28) {
+                    WheatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 76)
+                    WheatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 88)
+                    WheatherDayView(dayOfWeek: "THU", imageName: "wind", temperature: 70)
+                    WheatherDayView(dayOfWeek: "FRI", imageName: "cloud.bolt.fill", temperature: 65)
+                    WheatherDayView(dayOfWeek: "SAT", imageName: "cloud.rain.fill", temperature: 60)
+                }
+                
+                Spacer()
+                
+                Button {
+                    isNight.toggle()
+                } label: {
+                    WeatherButton(title: "Change Day Time", textColor: .white, backgroundColor: .mint)
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+struct WheatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .symbolRenderingMode(.multicolor)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)º")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue,  isNight ? .gray : Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .ignoresSafeArea()
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+
+struct MainWeatherStatusView: View {
+    
+    var isNight: Bool
+    
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(systemName: isNight ? "cloud.moon.fill" : "cloud.sun.fill")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            Text("\(temperature) º")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 40)
+    }
+}
+
+
